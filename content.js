a5d66632f73c9cc9d31e5ef29bacb565b2381569
Barkshihun <barkshihun@gmail.com>
2022-12-28 02:45:45 +0900

    ✨ Feat: placeholder와 title 속성에도 띄어쓰기 제거 추가

--- content.js
+++ content.js
@@ -1,8 +1,22 @@
 "use strict";
 const removeSpace = () => {
-  const treeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
-  while (treeWalker.nextNode()) {
-    const textNode = treeWalker.currentNode;
+  const placeholderTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: (node) => (node.placeholder ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
+  });
+  while (placeholderTreeWalker.nextNode()) {
+    const placeholderNode = placeholderTreeWalker.currentNode;
+    placeholderNode.placeholder = placeholderNode.placeholder.replace(/ /g, "");
+  }
+  const titlePropertyTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: (node) => (node.title ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
+  });
+  while (titlePropertyTreeWalker.nextNode()) {
+    const titlePropertyNode = titlePropertyTreeWalker.currentNode;
+    titlePropertyNode.title = titlePropertyNode.title.replace(/ /g, "");
+  }
+  const textNodeTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
+  while (textNodeTreeWalker.nextNode()) {
+    const textNode = textNodeTreeWalker.currentNode;
     const parentNodeName = textNode.parentNode.nodeName;
     if (parentNodeName === "SCRIPT" || parentNodeName === "STYLE") {
       continue;
