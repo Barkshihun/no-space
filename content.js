dff251882ed5e69085e80378e3e0bf054df51556
Barkshihun <barkshihun@gmail.com>
2024-02-23 22:54:29 +0900

    🎨 Format: removeSpace에서 트리워커와 와일문을 분리해서 정리

--- content.js
+++ content.js
@@ -3,27 +3,27 @@ const removeSpace = () => {
   const placeholderTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
     acceptNode: (node) => (node.placeholder ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
   });
+  const titlePropertyTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: (node) => (node.title ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
+  });
+  const valuePropertyTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: (node) => (node.value ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
+  });
+  const textNodeTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
   while (placeholderTreeWalker.nextNode()) {
     const placeholderNode = placeholderTreeWalker.currentNode;
     placeholderNode.placeholder = placeholderNode.placeholder.replace(/ /g, "");
   }
-  const titlePropertyTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
-    acceptNode: (node) => (node.title ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
-  });
   while (titlePropertyTreeWalker.nextNode()) {
     const titlePropertyNode = titlePropertyTreeWalker.currentNode;
     if (titlePropertyNode.attributes.title) {
       titlePropertyNode.attributes.title.textContent = titlePropertyNode.attributes.title.textContent.replace(/ /g, "");
     }
   }
-  const valuePropertyTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_ELEMENT, {
-    acceptNode: (node) => (node.value ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP),
-  });
   while (valuePropertyTreeWalker.nextNode()) {
     const valuePropertyNode = valuePropertyTreeWalker.currentNode;
     valuePropertyNode.value = valuePropertyNode.value.replace(/ /g, "");
   }
-  const textNodeTreeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
   while (textNodeTreeWalker.nextNode()) {
     const textNode = textNodeTreeWalker.currentNode;
     const parentNodeName = textNode.parentNode.nodeName;
